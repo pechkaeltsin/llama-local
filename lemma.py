02af7499abaafb91f634bcaf787b368905cda78b
pechkaeltsin <64319228+pechkaeltsin@users.noreply.github.com>
2024-08-02 12:12:49 +0300

    upd

--- lemma.py
+++ lemma.py
@@ -20,12 +20,17 @@ def preprocess(text, stop_words, punctuation_marks, morph):
     return " ".join(preprocessed_text)
 
 
-punctuation_marks = [",", ".", "(", ")", "-"]
-stop_words = stopwords.words("russian")
-morph = pymorphy3.MorphAnalyzer()
+def main():
+    punctuation_marks = [",", ".", "(", ")", "-"]
+    stop_words = stopwords.words("russian")
+    morph = pymorphy3.MorphAnalyzer()
 
-data = pd.read_csv("src/ksr.csv", sep=",")
+    data = pd.read_csv("src/ksr.csv", sep=",")
 
-data['name'] = data['name'].apply(lambda x: preprocess(x, stop_words, punctuation_marks, morph))
+    data['name'] = data['name'].apply(lambda x: preprocess(x, stop_words, punctuation_marks, morph))
 
-data.to_csv("src/ksr_lemma.csv", index=False, columns=['code', 'name', 'uint'])
+    data.to_csv("src/ksr_lemma.csv", index=False, columns=['code', 'name', 'uint'])
+
+
+if __name__ == "__main__":
+    main()
